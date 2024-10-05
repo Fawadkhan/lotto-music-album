f47df5f128b24b3ce6b7c9a5f188acfc9ee2a5da
fawad khan <fovadkhan@outlook.com>
2024-10-05 12:33:56 +0200

    feat: handle sorted array in filters

--- src/app/core/services/album.service.ts
+++ src/app/core/services/album.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable, signal } from '@angular/core';
 import { Album, SortCriteria } from 'src/app/core/models';
 import { MOCK_ALBUMS } from '../fixtures/mock-data';
@@ -7,39 +8,46 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class AlbumService {
-  private albumsSignal = signal<Album[]>([]);
+  private albumList = signal<Album[]>([]);
   private sortedAlbums: Album[] = [];
 
   constructor(private http: HttpClient) {
-    this.albumsSignal.set(MOCK_ALBUMS);
+    this.albumList.set(MOCK_ALBUMS);
     // uncomment the line below to use the endpoint created at mockapi.io instead of mock data
     // this.http.get<Album[]>(MOCK_ALBUM_ENDPOINT).subscribe(albums => {
     //   this.albumsSignal.set(albums);
     // });
   }
 
-  getAlbums = this.albumsSignal;
+  getAlbums = this.albumList;
 
   getAlbum(id: number) {
-    return this.albumsSignal().find(album => album.id === id);
+    return this.albumList().find(album => album.id === id);
   };
 
   sortAlbums(criteria: SortCriteria | undefined) {
     if(!criteria) return;
-    this.albumsSignal.update(albums => [...albums].sort((a, b) => a[criteria].localeCompare(b[criteria])))
+     this.albumList.update(albums => {
+      this.sortedAlbums = [...albums].sort((a, b) => a[criteria].localeCompare(b[criteria]))
+      return this.sortedAlbums;
+    })
   };
 
   filterAlbums(artist: string) {
+    let filteredAlbums: Album[] = []
     if (!artist) {
-      this.albumsSignal.set(MOCK_ALBUMS);
-    } else {
-      const filteredAlbums = this.sortedAlbums.filter(album => album.artist.toLowerCase().startsWith(artist.toLowerCase()));
-      this.albumsSignal.set(filteredAlbums);
+      return this.albumList.set(MOCK_ALBUMS);
     }
+    if(this.sortedAlbums.length > 0) {
+      filteredAlbums = this.sortedAlbums.filter(album => album.artist.toLowerCase().startsWith(artist.toLowerCase()));
+    } else {
+      filteredAlbums = MOCK_ALBUMS.filter(album => album.artist.toLowerCase().startsWith(artist.toLowerCase()));
+    }
+    this.albumList.set(filteredAlbums);
   };
 
   resetToOriginal() {
-    this.albumsSignal.set(MOCK_ALBUMS);
+    this.albumList.set(MOCK_ALBUMS);
   };
 
 }
